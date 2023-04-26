bf6eb5667ba7d53d9fb753b0cea1fd0487508124
Kristina Mancini <km36558p@pace.edu>
2023-04-26 15:20:32 -0400

    Add leetcode probem on min size subarray sum

---
+++ Arrays/Techniques/SlidingWindow/LeetCode209.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+class LeetCode209{
+    public int minSubArrayLen(int target, int[] nums) {
+        int size = nums.length;
+        if (size == 0)
+            return 0;
+
+        // two pointers, i and j
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int min_length = Integer.MAX_VALUE;
+
+        while (j < size){
+            sum = sum + nums[j++];
+
+            while (sum >= target){
+                min_length = Math.min(min_length, j - i);
+                sum = sum - nums[i++];
+            }
+        }
+        if (min_length == Integer.MAX_VALUE)
+          return 0;
+        else
+          return min_length;
+    }
+}
+
+/*
+Source Credit:
+https://leetcode.com/problems/minimum-size-subarray-sum/solutions/59078/accepted-clean-java-o-n-solution-two-pointers/?orderBy=most_votes
+
+This discussion thread has more replies with good explanations. 
+
+*/
