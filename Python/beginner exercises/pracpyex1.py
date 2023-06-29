7d093323be2d78c2823cd1e63fb706d9f694c07d
Kristina Mancini <km36558p@pace.edu>
2023-06-29 15:02:08 -0400

    Add new folders, add python exercise

---
+++ Python/beginner exercises/pracpyex1.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+print("Your name is: " + name)
+
+currentYear = int(input("Enter the current year: "))
+print("Current year: ", currentYear)
+
+age = int(input("Enter your age: "))
+print("Your age is: ", age)
+
+yearsUntil100 = 100 - age
+
+newYear = yearsUntil100 + currentYear
+
+print("You will turn 100 in the year of ", newYear )
