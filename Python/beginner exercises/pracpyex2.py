10551090e0528b795a5d3f3b0b06b5bd9ffec525
Kristina Mancini <km36558p@pace.edu>
2023-07-07 16:45:00 -0400

    Add python exercise

---
+++ Python/beginner exercises/pracpyex2.py
@@ -0,0 +1,13 @@
+'''
+Ask the user for a number. 
+Depending on whether the 
+number is even or odd, 
+print out an appropriate 
+message to the user.
+'''
+num = int(input('Please enter a number: '))
+
+if (num % 2 == 0):
+    print('Your number is even')
+else:
+    print('Your number is odd')
