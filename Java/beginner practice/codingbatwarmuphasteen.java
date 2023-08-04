f5e952a7b7a078eb2109fb2e3ef42dc3dc42f666
Kristina Mancini <km36558p@pace.edu>
2023-08-04 18:35:57 -0400

    Add java exercise

---
+++ Java/beginner practice/codingbatwarmuphasteen.java
@@ -0,0 +1,13 @@
+/*
+We'll say that a number is "teen" if it is in the range 13..19 
+inclusive. Given 3 int values, return true if 1 or more of them 
+are teen.
+*/
+
+public boolean hasTeen(int a, int b, int c) {
+  if (a > 12 && a < 20 || b > 12 && b < 20 || c > 12 && c < 20)
+    return true;
+  else
+    return false;
+  
+}
