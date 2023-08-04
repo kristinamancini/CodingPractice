515b0fc4d2968e850f4d2a019a38acaa654b7d68
Kristina Mancini <km36558p@pace.edu>
2023-08-03 21:46:59 -0400

    Add java exercise

---
+++ Java/beginner practice/codingbatwarmupstarthi.java
@@ -0,0 +1,11 @@
+/*
+Given a string, return true if the string
+starts with "hi" and false otherwise.
+*/
+
+public boolean startHi(String str) {
+  if (str.startsWith("hi"))
+    return true;
+  else
+    return false;
+}
