cffeccf6dd18de2b2435596a6db676a25cc5ddb2
Kristina Mancini <km36558p@pace.edu>
2023-08-23 10:22:47 -0400

    Add javascript exercise

---
+++ JavaScript/basic exercises/checkequalityvalues.js
@@ -0,0 +1,6 @@
+function checkEquality(a, b){
+    return a === b;
+}
+console.log("Checking the equality of two values...");
+console.log(checkEquality("r", 3));
+console.log("The types were", typeof "r", "and", typeof 3);
