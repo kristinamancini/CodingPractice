109b3b42b0b514c2ee517da2c7ea704f4ccb1c1d
Kristina Mancini <km36558p@pace.edu>
2023-08-26 06:19:35 -0400

    Add java exercise

---
+++ Java/beginner practice/codingbatwarmupdeldel.java
@@ -0,0 +1,18 @@
+/*
+Given a string, if the string "del" appears
+starting at index 1, return a string where
+that "del" has been deleted. Otherwise, return
+the string unchanged.
+*/
+
+public String delDel(String str) {
+if (str.length() >= 4) {
+if (str.substring(1, 4).equals("del"))
+return str.charAt(0) + str.substring(4);
+else
+return str;
+}
+else
+return str;
+
+}
