e38b55efdd04d5d997d8701a59a8819c304948c6
Kristina Mancini <km36558p@pace.edu>
2023-08-27 20:00:54 -0400

    Add java exercise

---
+++ Java/beginner practice/codingbatwarmupintmax.java
@@ -0,0 +1,12 @@
+/*
+Given three int values, a b c, return the largest.
+*/
+
+public int intMax(int a, int b, int c) {
+  if (a > b && a > c)
+    return a;
+  if (b > c && b > a)
+    return b;
+  else 
+    return c;
+}
