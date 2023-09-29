0cdce4beb981a60a90457eecfd24298c31bd38a8
Kristina Mancini <km36558p@pace.edu>
2023-09-29 08:29:40 -0400

    Add java exercise

---
+++ Java/beginner practice/codingbatstring1seecolor.java
@@ -0,0 +1,14 @@
+/*
+Given a string, if the string begins with
+"red" or "blue" return that color string,
+otherwise return the empty string.
+*/
+
+public String seeColor(String str) {
+	if (str.startsWith("red"))
+		return "red";
+	if (str.startsWith("blue"))
+		return "blue";
+	else
+		return "";
+}
