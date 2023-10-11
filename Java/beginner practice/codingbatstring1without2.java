8722174df802820cb93620c6d5ed6b0aa7aab4c3
Kristina Mancini <km36558p@pace.edu>
2023-10-11 00:01:23 -0400

    Add java exercise

---
+++ Java/beginner practice/codingbatstring1without2.java
@@ -0,0 +1,19 @@
+/*
+Given a string, if a length 2 substring appears
+at both its beginning and end, return a string
+without the substring at the beginning, so "HelloHe"
+yields "lloHe". The substring may overlap with itself,
+so "Hi" yields "". Otherwise, return the original string
+unchanged.
+*/
+
+public String without2(String str) {
+  if (str.length() == 2)
+    return "";
+  else if (str.length() <= 1)
+    return str;
+  else if (str.endsWith(str.substring(0, 2)))
+    return str.substring(2);
+  else
+    return str;
+}
