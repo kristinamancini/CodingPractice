1971695539090f1ec79253af2904e85a670c7a30
Kristina Mancini <km36558p@pace.edu>
2023-10-19 07:43:48 -0400

    Add java exercise

---
+++ Java/beginner practice/codingbatstring1atfirst.java
@@ -0,0 +1,15 @@
+/*
+Given a string, return a string length 2
+made of its first 2 chars. If the string
+length is less than 2, use '@' for the
+missing chars.
+*/
+
+public String atFirst(String str) {
+  if (str.isEmpty())
+    return "@@";
+  else if (str.length() < 2)
+    return str + "@";
+  else
+    return str.substring(0, 2);
+}
