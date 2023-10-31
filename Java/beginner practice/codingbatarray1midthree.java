bccb3d6ec0bf05835792afd37cab99fe059d3bc6
Kristina Mancini <km36558p@pace.edu>
2023-10-31 09:09:39 -0400

    Add java exercise - arrays

---
+++ Java/beginner practice/codingbatarray1midthree.java
@@ -0,0 +1,16 @@
+/*
+Given an array of ints of odd length,
+return a new array length 3 containing
+the elements from the middle of the array.
+The array length will be at least 3.
+*/
+
+public int[] midThree(int[] nums) {
+  int newArray[] = new int[3];
+
+  newArray[0] = nums[nums.length / 2 - 1];
+  newArray[1] = nums[nums.length / 2];
+  newArray[2] = nums[nums.length / 2 + 1];
+
+  return newArray;
+}
