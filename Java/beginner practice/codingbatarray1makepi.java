cdfc160112716028ad97bce5c60e68541a08a287
Kristina Mancini <km36558p@pace.edu>
2023-11-11 07:43:23 -0500

    Add java exercise - arrays

---
+++ Java/beginner practice/codingbatarray1makepi.java
@@ -0,0 +1,12 @@
+/*
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+*/
+
+public int[] makePi() {
+  int newArray[] = new int[3];
+  newArray[0] = 3;
+  newArray[1] = 1;
+  newArray[2] = 4;
+
+  return newArray;
+}
