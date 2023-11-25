68e26f527565c610130a0e9b0992183d44320074
Kristina Mancini <85172362+kristinamancini@users.noreply.github.com>
2023-11-25 09:31:47 -0500

    Fix java file

--- Java/beginner practice/codingbatlogic1caughtspeeding.java
+++ Java/beginner practice/codingbatlogic1caughtspeeding.java
@@ -1,17 +1,23 @@
 /*
-When squirrels get together for a party, they like to have
-cigars. A squirrel party is successful when the number of
-cigars is between 40 and 60, inclusive. Unless it is the
-weekend, in which case there is no upper bound on the number
-of cigars. Return true if the party with the given values
-is successful, or false otherwise.
+You are driving a little too fast, and a police officer stops
+you. Write code to compute the result, encoded as an int
+value: 0=no ticket, 1=small ticket, 2=big ticket. If speed
+is 60 or less, the result is 0. If speed is between 61 and
+80 inclusive, the result is 1. If speed is 81 or more, the
+result is 2. Unless it is your birthday -- on that day, your
+speed can be 5 higher in all cases.
 */
 
-public boolean cigarParty(int cigars, boolean isWeekend) {
-  if (cigars >= 40 && cigars <= 60)
-    return true;
-  else if (cigars >= 60 && isWeekend)
-    return true;
-  else
-    return false;
+public int caughtSpeeding(int speed, boolean isBirthday) {
+  int result = 0;
+
+  if (isBirthday && ((speed <= 86)))
+  result = 0;
+  else if (speed <= 60)
+    result = 0; //no ticket
+  else if (speed >= 61 && speed <= 80)
+    result = 1; //small ticket
+  else if (speed >= 81)
+    result = 2; //big ticket
+  return result;
 }
